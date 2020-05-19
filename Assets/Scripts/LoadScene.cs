63aef75cd97104812a8cd07907258f9e14af5a55
Nikolay <koli_y@mail.ru>
2020-05-20 04:21:10 +0500

    razberaisya sam

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class LoadScene : MonoBehaviour
 {
+    BoxCollider2D BC2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    private void OnMouseDown()
+    {
+
+        Application.LoadLevel("scene1");
+    }
+
 }
